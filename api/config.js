455094d35d40f38359fb93e791d06d62406a19fc
saranconstruction <info@saranconstruction.ca>
2026-06-11 00:05:38 -0400

    Create config.js

---
+++ api/config.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate=300');
+  res.status(200).json({
+    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+  });
+}
